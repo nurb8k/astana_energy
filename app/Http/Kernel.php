7648ea1b182a00f0c4eb7a821bb9ff554a7c0437
emdommed <nurb8k@gmail.com>
2023-10-27 02:17:47 +0600

    files added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Localization::class,
-            \App\Http\Middleware\setLocale::class,
+            \App\Http\Middleware\SetLocale::class,
         ],
 
         'api' => [
