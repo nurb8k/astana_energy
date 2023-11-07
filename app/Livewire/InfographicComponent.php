ac845082bad2a9a4ae6ef558b3468e4246b3d51e
emdommed <nurb8k@gmail.com>
2023-11-07 16:12:27 +0600

    compliance.blade.php translated file

--- app/Livewire/InfographicComponent.php
+++ app/Livewire/InfographicComponent.php
@@ -10,18 +10,19 @@ use Illuminate\Http\Request;
 class InfographicComponent extends Component
 {
     use WithPagination;
-    public $date = null;
+
+    public $date;
+
     public function render()
     {
-        if($this->date != null)
-            $data = Infographic::query()
-                ->where('date', '=',  $this->date )
-                ->orderBy('date', 'desc')
-                ->paginate(6);
-        else
-            $data = Infographic::query()
-                ->orderBy('date', 'desc')
-                ->paginate(6);
+        $query = Infographic::query()
+            ->orderBy('date', 'desc');
+
+        if ($this->date) {
+            $query->where('date', '=', $this->date);
+        }
+
+        $data = $query->paginate(6);
 
         return view('livewire.infographic', ['data' => $data]);
     }
