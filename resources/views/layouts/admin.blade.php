76044bccdb6ffe9862b52e2d040ffb641dbb6df9
emdommed <nurb8k@gmail.com>
2023-11-16 13:41:52 +0600

    asdasd

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -172,6 +172,7 @@
                            <span >{{Auth::user()->name}}</span>
                         </li>
 
+
                         <!-- User -->
                         <li class="nav-item navbar-dropdown dropdown-user dropdown">
                             <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
@@ -179,50 +180,21 @@
                                     <img src="{{asset('assets/img/avatars/1.png')}}" alt class="w-px-40 h-auto rounded-circle" />
                                 </div>
                             </a>
-                            <ul class="dropdown-menu dropdown-menu-end">
-                                <li>
-                                    <a class="dropdown-item" href="#">
-                                        <div class="d-flex">
-                                            <div class="flex-shrink-0 me-3">
-                                                <div class="avatar avatar-online">
-                                                    <img src="{{asset('assets/img/avatars/1.png')}}" alt class="w-px-40 h-auto rounded-circle" />
-                                                </div>
-                                            </div>
-                                            <div class="flex-grow-1">
-                                                <span class="fw-medium d-block">{{Auth::user()->name}}</span>
-{{--                                                <small class="text-muted">Admin</small>--}}
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li>
-                                    <div class="dropdown-divider"></div>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#">
-                                        <i class="bx bx-user me-2"></i>
-                                        <span class="align-middle">My Profile</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <div class="dropdown-divider"></div>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"  onclick="event.preventDefault();
+                        </li>
+                        <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"  onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        <i class="bx bx-power-off me-2"></i>
-                                        <span class="align-middle"> Выйти</span>
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </li>
-                            </ul>
+                                <i class="bx bx-power-off me-2"></i>
+                                <span class="align-middle"> Выйти</span>
+                            </a>
                         </li>
                         <!--/ User -->
                     </ul>
                 </div>
             </nav>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
 
             <!-- / Navbar -->
 
