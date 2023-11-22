7e3bf18f1e58f09083a772908aac9b5e1afad621
emdommed <nurb8k@gmail.com>
2023-11-22 12:17:01 +0600

    changed for security

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>АСТАНА ЭНЕРГИЯ | @yield('title')</title>
     <meta charset="UTF-8">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self' https://apis.google.com">
+{{--    <meta http-equiv="Content-Security-Policy" content="default-src 'self' https://apis.google.com">--}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Астана Энергия</title>
     <link rel="stylesheet" href="{{asset('user/assets/css/global.css')}}">
