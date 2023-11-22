eab9c391ad5f036a6e1ddefa93564771f44d38e3
emdommed <nurb8k@gmail.com>
2023-11-22 16:47:19 +0600

    translations

--- resources/views/pages/documentation/rules.blade.php
+++ resources/views/pages/documentation/rules.blade.php
@@ -6,42 +6,42 @@
         <div class="govs-inner">
             <h2>Заң шығару және нормативтiк актiлер</h2>
             <div class="gov__description">
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/Z1500000401#z1" target="_blank">Ақпаратқа қол жеткізу туралы</a></p>
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/Z1500000401#z1" target="_blank">Ақпаратқа қол жеткізу туралы</a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/Z1500000402#z33" target="_blank">Қайырымдылық туралы</a></p>
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/Z1500000402#z33" target="_blank">Қайырымдылық туралы</a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/Z1300000088" target="_blank">Мемлекеттік көрсетілетін қызметтер туралы</a></p>
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/Z1300000088" target="_blank">Мемлекеттік көрсетілетін қызметтер туралы</a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/Z040000586_" target="_blank">
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/Z040000586_" target="_blank">
                         Қазақстан Республикасының кейбір заңнамалық актілеріне зияткерлік меншік мәселелері бойынша өзгерістер мен толықтырулар енгізу туралы
                     </a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/V1800017561" target="_blank">Мемлекет кепілдік берген заң көмегі туралы
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/V1800017561" target="_blank">Мемлекет кепілдік берген заң көмегі туралы
 
                     </a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/Z000000107_" target="_blank">Әкімшілік рәсімдер туралы</a></p>
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/Z000000107_" target="_blank">Әкімшілік рәсімдер туралы</a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/Z1500000418" target="_blank">Ақпараттандыру туралы</a></p>
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/Z1500000418" target="_blank">Ақпараттандыру туралы</a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/Z990000451_" target="_blank">
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/Z990000451_" target="_blank">
                         Бұқаралық ақпарат құралдары туралы</a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/Z070000221_" target="_blank">Жеке және заңды тұлғалардың өтiнiштерiн қарау тәртiбi туралы</a></p>
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/Z070000221_" target="_blank">Жеке және заңды тұлғалардың өтiнiштерiн қарау тәртiбi туралы</a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/Z1500000434" target="_blank">Мемлекеттiк сатып алу туралы</a></p>
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/Z1500000434" target="_blank">Мемлекеттiк сатып алу туралы</a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/K1500000414" target="_blank">Қазақстан Республикасының Еңбек Кодексі</a></p>
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/K1500000414" target="_blank">Қазақстан Республикасының Еңбек Кодексі</a></p>
 
-                <p><a  class="text-primary" href="https://adilet.zan.kz/rus/docs/Z1500000410" target="_blank">Сыбайлас жемқорлыққа қарсы күрес туралы</a></p>
+                <p><a  class="text-primary" href="https://adilet.zan.kz/kaz/docs/Z1500000410" target="_blank">Сыбайлас жемқорлыққа қарсы күрес туралы</a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/K080000095_" target="_top">Қазақстан Республикасының Бюджет кодексі</a></p>
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/K080000095_" target="_top">Қазақстан Республикасының Бюджет кодексі</a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/K950001000_" target="_blank">ҚР Конституциясы</a></p>
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/K950001000_" target="_blank">ҚР Конституциясы</a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/Z970000151_" target="_blank">Қазақстан Республикасындағы тiл туралы</a></p>
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/Z970000151_" target="_blank">Қазақстан Республикасындағы тiл туралы</a></p>
 
-                <p><a class="text-primary" href="https://adilet.zan.kz/rus/docs/Z1500000416" target="_blank">ҚР "Мемлекеттік қызмет туралы" заңы
+                <p><a class="text-primary" href="https://adilet.zan.kz/kaz/docs/Z1500000416" target="_blank">ҚР "Мемлекеттік қызмет туралы" заңы
 
                     </a></p>
             </div>
